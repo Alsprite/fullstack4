8c83e47a8021b241c381cb62aaf70b9279a3d2e8
Alsprite <henri.mykra@esedulainen.fi>
2023-08-09 09:36:28 +0300

    13.4. vissii toimii en tiiä miten kokeilla

--- index.js
+++ index.js
@@ -38,7 +38,42 @@ app.get('/api/blogs', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
   })
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
+
+app.use(express.json())
+
+app.post('/api/blogs', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body)
+        return res.json(blog)
+    } catch (error) {
+        return res.status(400).json({error})
+    }
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id)
+    if (blog) {
+        console.log(blog.toJSON())
+        res.json(blog)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+    try {
+        const blog = await Blog.findByPk(req.params.id)
+        if (!blog) {
+            return res.status(404).json({ error: 'Blog not found' })
+        }
+        await blog.destroy()
+        return res.status(204).end()
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
