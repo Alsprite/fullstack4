2916054ef9d92c6faa82b0a38801380f9324af0f
Alsprite <henri.mykra@esedulainen.fi>
2023-09-13 11:25:50 +0300

    13.19. toimii?

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const List = require('./list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: List })
+Blog.belongsToMany(User, { through: List })
+
 module.exports = {
   Blog,
-  User
+  User,
+  List
 }
