c52d35a371c5c22839e9b7ea1febcc9c91c69e95
Alsprite <henri.mykra@esedulainen.fi>
2023-09-22 12:22:49 +0300

    session toimii

--- controllers/login.js
+++ controllers/login.js
@@ -1,51 +1,60 @@
 const jwt = require('jsonwebtoken')
 const router = require('express').Router()
-
 const { SECRET } = require('../util/config')
-const { User, Session } = require('../models')
+const { User } = require('../models')
+const { Session } = require('../models/session')
 
-router.post('/', async (request, response) => {
-  
-  const body = request.body
-
-  const user = await User.findOne({
-    where: {
-      username: body.username
+router.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    const user = await User.findOne({
+      where: {
+        username: body.username
+      }
+    })
+    const passwordCorrect = body.password === 'salainen'
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: 'Invalid username or password!'
+      })
     }
-  })
-  if (user.disabled) {
-    return response.status(401).json({
-      error: 'User disabled!'
+    if ((user.disabled)) {
+      return res.status(401).json({
+        error: 'User disabled!'
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user.id,
+    }
+    const token = jwt.sign(userForToken, SECRET)
+
+    await Session.destroy({
+      where: {
+        userId: user.id
+      }
+    })
+    await Session.create({
+      userId: user.id,
+      token: token,
+    })
+
+    res
+      .status(200)
+      .send({
+        token,
+        username: user.username,
+        name: user.name
+      })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(401).json({
+      error: 'Log in failed! Check username and password!'
     })
   }
-  const passwordCorrect = body.password === 'salainen'
-
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
-    })
-  }
-
-  const userForToken = {
-    username: user.username,
-    id: user.id,
-  }
-
-  const token = jwt.sign(userForToken, SECRET)
-
-  await Session.destroy({
-    where: {
-      userId: user.id
-    }
-  })
-  await Session.create({
-    userId: user.id,
-    token: token,
-  })
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name })
 })
+
 
 module.exports = router
